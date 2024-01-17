f40fd53fc7e9028910664eab781bce86bde200bb
vmarasinkiy <vmarasinkiy@gmail.com>
2024-01-17 16:22:27 +0300

    add sql test_8.1.1

--- cypress/e2e/sql.cy.js
+++ cypress/e2e/sql.cy.js
@@ -6,6 +6,23 @@ describe('connect to test db', () => {
     );
   });
 
+  it("Input entries", () => {
+    cy.task("queryDb",
+      `INSERT INTO Students (StudentID, FirstName, StudentGroup, City) VALUES
+      (1, 'Ivan', '02-2022', 'Barcelona'),
+      (2, 'Elena', '02-2022', 'Madrid'),
+      (3, 'Alex', '02-2022', 'Barcelona'),
+      (4, 'Maria', '02-2022', 'Madrid'),
+      (5, 'Dmitriy', '02-2022', 'Barcelona');`
+    );
+  });
+
+  it('can fetch all students from the group', () => {
+    cy.task('queryDb', 'SELECT * FROM Students WHERE StudentGroup = "02-2022"').then((result) => {
+    cy.log(JSON.stringify(result));
+    });
+    });
+
   it("can delete the db", () => {
     cy.task("queryDb", "DROP TABLE Students");
   });
