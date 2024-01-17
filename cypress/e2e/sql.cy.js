faf215c034260254142cc18b5c2bf6f0eb119a63
vmarasinkiy <vmarasinkiy@gmail.com>
2024-01-17 16:01:12 +0300

    update

--- cypress/e2e/sql.cy.js
+++ cypress/e2e/sql.cy.js
@@ -1,5 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('connect to test db', () => {
+  it('can connect to the db', () => {
+    cy.task(
+      "queryDb",
+      "CREATE TABLE Students(StudentID int, FirstName varchar(255), StudentGroup varchar(255), City varchar(255))"
+    );
+  });
+
+  it("can delete the db", () => {
+    cy.task("queryDb", "DROP TABLE Students");
+  });
+});
